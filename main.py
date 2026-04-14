82bf284df1dc4087dbcca188442b30707cae45da
chaunceybuey <chaunceybuey@gmail.com>
2026-04-13 20:40:11 -0500

    Change log level from 'critical' to 'info'

--- main.py
+++ main.py
@@ -292,7 +292,7 @@ if __name__ == "__main__":
     import os
 
     def run_server():
-        uvicorn.run(app, host="127.0.0.1", port=8088, log_level="critical")
+        uvicorn.run(app, host="127.0.0.1", port=8088, log_level="info")
 
     def prewarm_cache():
         import time
